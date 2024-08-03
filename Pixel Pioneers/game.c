bc7e31fc6cca90b7e65a9d3d890b1b7090e7e7c8
samot <samotgameing@gmail.com>
2024-08-03 21:36:58 +1000

    aaaaa

--- Pixel Pioneers/game.c
+++ Pixel Pioneers/game.c
@@ -498,6 +498,23 @@ Point enemy_movement(Point player, Point enemy, World* worldarray)
 	}
 	else
 		return enemy;
+	/*
+	Point start = {0, 0};
+	Point end = {7, 7};
+	Node* path = a_star(enemy, player, worldarray, gridWidth, gridHeight);
+
+	 if (path == NULL || path->parent == NULL)
+	{
+		Point invalid = {-1, -1}; // Return an invalid point if there's no path or the path is already at the destination
+		printf("error: game.c enem_movement(): invalid point, no path or the path is already at the destination \n");;
+		return save_enemy;
+	}
+	while (path->parent->parent != NULL)
+	{
+		path = path->parent;
+	}
+	return path->position;
+*/
 }
 leveldata levels(void)
 {
